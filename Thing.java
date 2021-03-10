d587c43328b6c71bd6a154b6e4ba779b684ed41b
mylesmrob <mylesmrobinson@gmail.com>
2021-03-10 13:17:16 -0500

    this is my first git upload from java

---
+++ Thing.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class Thing {
+
+    public static void main(String[] args) {
+        System.out.println("I got my first bike in the hood from crackhead Billy for $25");
+    }
+}
